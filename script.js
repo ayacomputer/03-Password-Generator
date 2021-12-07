a7f16b7f54dd97e19de0feab5be7ec498305c093
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-07 21:49:03 +1100

    updating javascript

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialChar = ['!', '@', '$', '%', '^', '&', '*', '(', ')', '+', '=', '?', ',']
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = '§!@#$%^&*()_+?><{}[]'.split();
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split();
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split();
+var number = '0123456789'.split();
+var selection = ｛
+  sp: 0,
+  low: 0,
+  up: 0,
+  length: 0
+｝
 
 
 // Write password to the #password input
@@ -17,18 +23,37 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var length = prompt("Choose the password length(between 8 - 128 characters)")
+  var inputLength = prompt("Choose the password length(8 - 128 characters)")
   if (!Number.isNaN) {
-    alert("Please enter a number.")
-    length();
-  } else if (length < 8 || length > 128) {
-    alert("The number is too small or too big.")
-    length();
+    alert("Please enter a valid number.");
+    generatePassword();
+  } else if (inputLength < 8 || inputLength > 128) {
+    alert("The number is too small or too big.");
+    generatePassword();
+  } else {
+    length += inputLength;
   }
 
-  var specialChar = confirm("Do you want special characters?")
+  var spCharConfirm = confirm("Do you want special characters?");
+  if (spCharConfirm) {
+    sp++;
+  }
+  var lowCharConfirm = confirm("Do you want special characters?");
+  if (lowCharConfirm) {
+    low++;
+  }
+  var upCharConfirm = confirm("Do you want special characters?");
+  if (upCharConfirm) {
+    up++;
+  }
+
+  if (sp: 0 && low: 0 && up: 0) {
+
+  }
 }
 
+
 generatePassword();
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
