bc795b11c1e1d84b6b9001db52c3c0457ced525a
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-08 09:50:22 +1100

    updating javascript fixing bugs

--- script.js
+++ script.js
@@ -5,62 +5,64 @@ var specialChar = '§!@#$%^&*()_+?><{}[]'
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var number = '0123456789'
-var choice = ""
-var randomPassword = "";
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 function generatePassword() {
+  var randomPassword = "";
+  var choice = "";
   var pwLength = prompt("Choose the password length(8 - 128 characters)")
-  if (!Number.isNaN) {
+  if (isNaN(pwLength)) {
     alert("Please enter a valid number.");
     generatePassword();
+    return;
   } else if (pwLength < 8 || pwLength > 128) {
     alert("The number is too small or too big.");
     generatePassword();
+    return;
   }
 
   var spCharConfirm = confirm("Do you want to include special characters?");
   if (spCharConfirm) {
     choice += specialChar;
+    randomPassword += specialChar.charAt(Math.floor(Math.random() * specialChar.length));
   }
   var lowCharConfirm = confirm("Do you want to include lowercase alphabets?");
   if (lowCharConfirm) {
     choice += lowerCase;
+    randomPassword += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
   }
   var upCharConfirm = confirm("Do you want to include uppercase alphabets?");
   if (upCharConfirm) {
     choice += upperCase;
+    randomPassword += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
   }
   var numConfirm = confirm("Do you want to include numbers?");
   if (numConfirm) {
     choice += number;
+    randomPassword += number.charAt(Math.floor(Math.random() * number.length));
   }
   if (!spCharConfirm && !lowCharConfirm && !upCharConfirm && !numConfirm) {
-    alert("We need some characters to make your password");
+    alert("We need some characters to make your password.");
     generatePassword();
+    return;
   }
   for (let i = 0; i < pwLength; i++) {
-    randomPassword += choice.charAt(Math.floor(Math.random() * pwLength));
+    randomPassword += choice.charAt(Math.floor(Math.random() * choice.length));
   }
+  console.log(randomPassword);
+  console.log(choice);
   return randomPassword;
 }
-generatePassword();
 
-// Add event listener to generate button
 
-generateBtn.addEventListener("click", function () {
-  console.log('This button works');
-  document.getElementById('#password').innerHTML = randomPassword;
-  console.log(choice);
-  console.log(randomPassword);
-  console.log(password);
-});
+
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword)
